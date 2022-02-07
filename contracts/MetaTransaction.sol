203fdf3283417ce5b6d0f01188df310ff64b10b8
komus-Israel <komolehinIsrael@gmail.com>
2022-02-07 21:24:06 +0100

    initialized the contract and created a file for the unit test

--- contracts/MetaTransaction.sol
+++ contracts/MetaTransaction.sol
@@ -1,1 +1,26 @@
 pragma solidity 0.8.10;
+
+
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
+
+contract MetaTransaction {
+
+    struct certificate {
+
+        uint256 nonce;
+        uint256 maxAmount;
+        uint256 minAmount;
+        address signer;
+        
+
+    }
+
+    function signCertificate () returns (bytes) {
+
+    }
+
+}
