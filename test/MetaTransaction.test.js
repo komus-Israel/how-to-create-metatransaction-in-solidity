eacedd67d806a137e6b0de394f9955fb5890bd81
komus-Israel <komolehinIsrael@gmail.com>
2022-02-08 10:52:39 +0100

    hash changed when a message was altered

--- test/MetaTransaction.test.js
+++ test/MetaTransaction.test.js
@@ -71,7 +71,7 @@ contract("Meta", ([address1, address2, operator1, operator2])=>{
 
             nonce: 225,
             maxAmount: 1000,
-            minAmount: 10
+            minAmount: 1
 
         }
 
